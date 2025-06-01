300baa15f1a371749f1cfe9eb22fed13d64bca86
Ezra <ezychaitea@gmail.com>
2025-06-01 23:16:34 +1200

    Initial Commit

---
+++ src/main/java/com/ezra/coordinatedisplay/CoordinatesDisplayClient.java
@@ -0,0 +1,12 @@
+package com.ezra.coordinatedisplay;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CoordinatesDisplayClient implements ClientModInitializer{
+
+	@Override
+	public void onInitializeClient() {
+		
+	}
+
+}
---
+++ src/main/java/com/ezra/coordinatedisplay/CoordinatesDisplayDataGenerator.java
@@ -0,0 +1,11 @@
+package com.ezra.coordinatedisplay;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CoordinatesDisplayDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
